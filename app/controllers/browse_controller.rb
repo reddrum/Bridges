b8b5dc09f44406e8e91c6251bb0fc147344d75cd
“reddrum” <anton.kranz@me.com>
2019-11-24 21:53:46 +0300

    Edited views&styles

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -4,9 +4,6 @@ class BrowseController < ApplicationController
     liked_account_ids << current_account.id
 
     @users = Account.where.not(id: liked_account_ids)
-  end
-
-  def matches
     @matches = current_account.matches
   end
 
